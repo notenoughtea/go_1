712826eff7e3984b545d7b02f02699b76eac150d
Alan <dreamwalk@mail.ru>
2025-06-12 19:31:10 +0300

    v2

--- main.go
+++ main.go
@@ -15,34 +15,138 @@ type obj struct {
 }
 
 type player struct {
-	place      location
-	insventory []item
+	place       location
+	hasBackpack bool
+	inventory   []item
 }
 
 type item struct {
 	title string
 }
 
-func (p player) look(loc location) string {
-	answer := ""
-	if p.place.title == "кухня" {
-		answer += "ты находишься на кухне"
+func findPaths(loc location) string {
+	result := "можно пройти - "
+	for n := 0; n < len(loc.paths); n++ {
+		path := loc.paths[n]
+		if n < len(loc.paths)-1 {
+			fmt.Println(n)
+			result += path + ", "
+		} else {
+			result += path
+		}
 	}
+	return result
+}
+
+func findObjectItems(loc location) string {
+	result := ""
 	if len(loc.objects) > 0 {
-		for _, obj := range loc.objects {
-			if obj.title == "стол" {
-				answer += ", на столе: "
-				for _, item := range obj.items {
-					answer += item.title + ", "
+		for i, obj := range loc.objects {
+			result += "на " + obj.title + ": "
+			for i, item := range obj.items {
+				if i < len(obj.items)-1 {
+					result += item.title + ", "
+				} else {
+					result += item.title
 				}
+			}
+			if i < len(loc.objects)-1 {
+				result += ", "
 			}
 		}
 	}
-	answer += ", надо собрать рюкзак и идти в универ. можно пройти - "
-	for _, path := range loc.paths {
-		answer += path + ", "
+	return result
+}
+
+func (p player) look(loc location) string {
+	result := ""
+	switch p.place.title {
+	case "кухня":
+		answer := "ты находишься на кухне, "
+		answer += findObjectItems(loc) + ", "
+		answer += "надо собрать рюкзак и идти в универ. "
+		answer += findPaths(loc)
+		result = answer
+
+	case "корридор":
+		answer := "ты находишься на " + p.place.title + ", "
+		answer += findObjectItems(loc) + ", "
+		answer += "надо собрать рюкзак и идти в универ. "
+		answer += findPaths(loc)
+		result = answer
+
+	case "комната":
+		answer := findObjectItems(loc) + ", "
+		answer += findPaths(loc)
+		result = answer
+
+	case "улица":
+		answer := "ты находишься на " + p.place.title + ", "
+		answer += findObjectItems(loc) + ", "
+		answer += "надо собрать рюкзак и идти в универ. "
+		answer += findPaths(loc)
+		result = answer
 	}
-	return answer
+	return result
+}
+
+func (p player) move(loc location) string {
+	if loc.locked {
+		return "дверь закрыта"
+	}
+	result := ""
+	switch loc.title {
+	case "кухня":
+		answer := "кухня, ничего интересного. "
+		answer += findPaths(loc)
+		result = answer
+		p.place = loc
+
+	case "корридор":
+		answer := "ничего интересного, "
+		answer += findPaths(loc)
+		result = answer
+		p.place = loc
+
+	case "комната":
+		answer := "ты в своей комнате, "
+		answer += findPaths(loc)
+		result = answer
+		p.place = loc
+
+	case "улица":
+		result = "на улице весна. можно пройти - домой"
+	}
+	return result
+}
+
+func (p player) take(item string, loc location) string {
+	result := ""
+	for n := 0; n < len(loc.objects); n++ {
+		for i := 0; i < len(loc.objects[n].items); i++ {
+			if (loc.objects[n].items[i].title == item) && (loc.objects[n].items[i].title == "рюкзак") {
+				result := "вы надели: рюкзак"
+				p.inventory = append(p.inventory, loc.objects[n].items[i])
+				p.inventory = append(p.inventory, loc.objects[n].items[i])
+				p.hasBackpack = true
+				return result
+			}
+			if (loc.objects[n].items[i].title == item) && (p.hasBackpack == true) {
+				result := "предмет добавлен в инвентарь: " + item
+				p.inventory = append(p.inventory, loc.objects[n].items[i])
+				loc.objects[n] = Delete(i, i)
+				return result
+			}
+			if (loc.objects[n].items[i].title == item) && (p.hasBackpack == false) {
+				result := "нет места"
+				return result
+			} else {
+				result := "нет такого"
+				return result
+			}
+		}
+	}
+	return result
 }
 
 var items = []item{
@@ -92,12 +196,9 @@ var locations = []location{
 	},
 }
 
-func initGame(locs []location) {
-	locsInstance := locs
-	var hero player = player{place: locs[0]}
-	fmt.Println(hero.look(locsInstance[0]))
-}
-
-func main() {
-	initGame(locations)
+func initGame() {
+	instance := locations
+	var hero player = player{place: instance[2]}
+	//fmt.Println(hero.look(locs[2]))
+	fmt.Println(hero.move(instance[0]))
 }
